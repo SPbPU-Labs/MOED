105fb46942bf0f11198c157910d7b2273e2dd7b2
Artem Kurdikov <kourtema@gmail.com>
2024-09-04 18:35:09 +0300

    Lab1 part #* done

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -8,15 +8,41 @@ a_exp = 0.008
 b = 0
 delta = 1
 N = 1000
+C = 200
+
+model = Model()
+linear_up_data = model.trend(TrendFuncs.linear_func, a_linear, b, delta, N)
+linear_down_data = model.trend(TrendFuncs.linear_func, -a_linear, b, delta, N)
+exponential_up_data = model.trend(TrendFuncs.exp_func, -a_exp, b, delta, N)
+exponential_down_data = model.trend(TrendFuncs.exp_func, a_exp, b, delta, N)
 
 
 def test_trend_method():
-    linear_up_data = model.trend(TrendFuncs.linear_func, a_linear, b, delta, N)
-    linear_down_data = model.trend(TrendFuncs.linear_func, -a_linear, b, delta, N)
-    exponential_up_data = model.trend(TrendFuncs.exp_func, -a_exp, b, delta, N)
-    exponential_down_data = model.trend(TrendFuncs.exp_func, a_exp, b, delta, N)
-
     for data, i in zip([linear_up_data, linear_down_data, exponential_up_data, exponential_down_data], range(1, 5)):
+        plt.subplot(2, 2, i)
+        line_plot = sns.lineplot(x=np.arange(N), y=data)
+        if i % 2 != 0:
+            plt.title('Linear Uptrend')
+        else:
+            plt.title('Linear Downtrend')
+        line_plot.set_xlabel('t', fontsize=12)
+        line_plot.set_ylabel('x(t)', fontsize=12)
+
+def test_shift_method():
+    for data, i in zip([linear_up_data, linear_down_data, exponential_up_data, exponential_down_data], range(1, 5)):
+        data = model.shift(data, N, C)
+        plt.subplot(2, 2, i)
+        line_plot = sns.lineplot(x=np.arange(N), y=data)
+        if i % 2 != 0:
+            plt.title('Linear Uptrend')
+        else:
+            plt.title('Linear Downtrend')
+        line_plot.set_xlabel('t', fontsize=12)
+        line_plot.set_ylabel('x(t)', fontsize=12)
+
+def test_mult_method():
+    for data, i in zip([linear_up_data, linear_down_data, exponential_up_data, exponential_down_data], range(1, 5)):
+        data = model.mult(data, N, C)
         plt.subplot(2, 2, i)
         line_plot = sns.lineplot(x=np.arange(N), y=data)
         if i % 2 != 0:
@@ -28,13 +54,13 @@ def test_trend_method():
 
 
 if __name__ == '__main__':
-    model = Model()
-
     sns.set(style="whitegrid")
 
-    for func, i in zip([test_trend_method], range(1,2)):
-        plt.figure(figsize=(14, 8)).suptitle(f"Page #{i}")
+    for func, i in zip([test_trend_method, test_shift_method, test_mult_method], range(1, 4)):
+        fig = plt.figure(figsize=(14, 8))
+        fig.suptitle(f"Page #{i}")
         func()
+        fig.savefig(f"./plots/fig_{i}.png")
 
     plt.tight_layout()
     plt.show()
--- Lab1/src/model.py
+++ Lab1/src/model.py
@@ -6,7 +6,7 @@ class Model:
     def trend(self, func: Callable[[np.ndarray, float, float], np.ndarray], a: float, b: float, delta: float,
               N: int) -> np.ndarray:
         """
-        Генерирует трендовые данные на основе функции.
+        Генерация трендовые данные на основе функции.
         :param func: Функция для генерации данных тренда. Принимает параметры a, t (массив времени) и b, возвращает массив данных.
         :param a: Параметр a для функции тренда.
         :param b: Параметр b для функции тренда.
@@ -16,6 +16,32 @@ class Model:
         """
         t = np.arange(0, N * delta, delta)
         return func(t, a, b)
+
+    def shift(self, data: np.ndarray, N: int, C: float) -> np.ndarray:
+        """
+        Смещение данных на значение С.
+        :param data: Данные для смещения (массив numpy).
+        :param N: Количество элементов массива данных.
+        :param C: Значение смещения.
+        :return: Массив данных numpy смещённый на значение С.
+        :raises ValueError: Если размер массива данных != N.
+        """
+        if len(data) != N:
+            raise ValueError("Размерность массива не соответствует N")
+        return data + C
+
+    def mult(self, data: np.ndarray, N: int, C: float) -> np.ndarray:
+        """
+        Умножение данных на значение С
+        :param data: Данные для умножения (массив numpy).
+        :param N: Количество элементов массива данных.
+        :param C: Значение умножения.
+        :return: Массив данных numpy умноженный на значение С.
+        :raises ValueError: Если размер массива данных != N.
+        """
+        if len(data) != N:
+            raise ValueError("Размерность массива не соответствует N")
+        return data * C
 
 
 class TrendFuncs:
