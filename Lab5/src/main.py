72b5cdd293c2534a4ca721bf63194bd64f0a650c
Artem Kurdikov <kourtema@gmail.com>
2024-10-30 23:19:24 +0300

    lab5: modified plots

--- Lab5/src/main.py
+++ Lab5/src/main.py
@@ -7,8 +7,8 @@ from src.model import Model, TrendFuncs
 
 def make_line_plot(title: str, x: np.ndarray, y: np.ndarray):
     plot = sns.lineplot(x=x, y=y)
-    plot.set_xlabel('N', fontsize=12)
-    plot.set_ylabel('x(t)', fontsize=12)
+    plot.set_xlabel('Time [sec]', fontsize=12)
+    plot.set_ylabel('Amplitude', fontsize=12)
     plt.title(title)
     return plot
 
@@ -17,10 +17,11 @@ def test_harm_method():
     A0 = 100
     f = [15 + 100 * i for i in range(6)]
     dt = 0.001
+    time = np.arange(0, N*dt, dt)
     for i in range(len(f)):
         plt.subplot(3, 2, i+1)
         data = Model.harm(N, A0, f[i], dt)
-        make_line_plot(f'f_0 = {f[i]}', np.arange(N), data)
+        make_line_plot(f'f_0 = {f[i]}', time, data)
 
 def test_polyHarm_method():
     N = 1000
@@ -28,8 +29,10 @@ def test_polyHarm_method():
     A = [100, 15, 20]
     f = [33, 5, 170]
     dt = 0.002
+
     data = Model.polyHarm(N, A, f, M, dt)
-    make_line_plot('', np.arange(N), data)
+    time = np.arange(0, N*dt, dt)
+    make_line_plot('', time, data)
     print(f'Частота полигармонического процесса: {np.max(f)}')
 
 def test_addModel_method():
@@ -40,27 +43,27 @@ def test_addModel_method():
     f = 50
     dt = 0.002
     N = 1000
-    t = np.arange(0.0, N*dt, dt)
 
-    data1 = TrendFuncs.linear_func(t, a[0], b[0])
+    time = np.arange(0, N*dt, dt)
+    data1 = TrendFuncs.linear_func(time, a[0], b[0])
     data2 = Model.harm(N, A, f, dt)
     result = Model.addModel(data1, data2)  # linear trend + harm
     plt.subplot(2, 3, 1)
-    make_line_plot('Linear trend + Harm', np.arange(N), result)
+    make_line_plot('Linear trend + Harm', time, result)
     plt.subplot(2, 3, 2)
-    make_line_plot('Linear trend', np.arange(N), data1)
+    make_line_plot('Linear trend', time, data1)
     plt.subplot(2, 3, 3)
-    make_line_plot('Harm', np.arange(N), data2)
+    make_line_plot('Harm', time, data2)
 
-    data1 = TrendFuncs.exp_func(t, a[1], b[1])
+    data1 = TrendFuncs.exp_func(time, a[1], b[1])
     data2 = Model.noise(N, R)
     result = Model.addModel(data1, data2)  # exp trend + noise
     plt.subplot(2, 3, 4)
-    make_line_plot('Exp trend + Noise', np.arange(N), result)
+    make_line_plot('Exp trend + Noise', time, result)
     plt.subplot(2, 3, 5)
-    make_line_plot('Exp trend', np.arange(N), data1)
+    make_line_plot('Exp trend', time, data1)
     plt.subplot(2, 3, 6)
-    make_line_plot('Noise', np.arange(N), data2)
+    make_line_plot('Noise', time, data2)
 
 def test_multModel_method():
     a = [0.3, 0.05]
@@ -70,27 +73,27 @@ def test_multModel_method():
     f = 50
     dt = 0.002
     N = 1000
-    t = np.arange(0.0, N*dt, dt)
 
-    data1 = TrendFuncs.linear_func(t, a[0], b[0])
+    time = np.arange(0.0, N*dt, dt)
+    data1 = TrendFuncs.linear_func(time, a[0], b[0])
     data2 = Model.harm(N, A, f, dt)
     result = Model.multModel(data1, data2)  # linear trend + harm
     plt.subplot(2, 3, 1)
-    make_line_plot('Linear trend + Harm', np.arange(N), result)
+    make_line_plot('Linear trend + Harm', time, result)
     plt.subplot(2, 3, 2)
-    make_line_plot('Linear trend', np.arange(N), data1)
+    make_line_plot('Linear trend', time, data1)
     plt.subplot(2, 3, 3)
-    make_line_plot('Harm', np.arange(N), data2)
+    make_line_plot('Harm', time, data2)
 
-    data1 = TrendFuncs.exp_func(t, a[1], b[1])
+    data1 = TrendFuncs.exp_func(time, a[1], b[1])
     data2 = Model.noise(N, R)
     result = Model.multModel(data1, data2)  # exp trend + noise
     plt.subplot(2, 3, 4)
-    make_line_plot('Exp trend + Noise', np.arange(N), result)
+    make_line_plot('Exp trend + Noise', time, result)
     plt.subplot(2, 3, 5)
-    make_line_plot('Exp trend', np.arange(N), data1)
+    make_line_plot('Exp trend', time, data1)
     plt.subplot(2, 3, 6)
-    make_line_plot('Noise', np.arange(N), data2)
+    make_line_plot('Noise', time, data2)
 
 
 if __name__ == '__main__':
