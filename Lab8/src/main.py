4a0dbd4f599accb5fb28bda7c9875f909a7b53c7
Artem Kurdikov <kourtema@gmail.com>
2024-10-31 00:24:40 +0300

    lab8: implemented antiSpike method

---
+++ Lab8/src/main.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import os
+
+from processing import Processing
+from src.model import Model
+
+
+def make_line_plot(title: str, x: np.ndarray, y: np.ndarray, x_label="Time [sec]", y_label="Amplitude"):
+    plot = sns.lineplot(x=x, y=y)
+    plot.set_xlabel(x_label, fontsize=12)
+    plot.set_ylabel(y_label, fontsize=12)
+    plt.title(title)
+    return plt
+
+
+def test_antiSpike_method():
+    global fig
+
+    def __noise_and_spikes_add():
+        data1 = Model.noise(N, R)
+        data2 = Model.spikes(N, M, 10*R, R_s)
+        add_model = Model.addModel(data1, data2)
+        result = Processing.antiSpike(add_model, N, R)
+
+        plt.subplot(2, 2, 1)
+        make_line_plot('Noise', time, data1)
+        plt.subplot(2, 2, 2)
+        make_line_plot('Spikes', time, data2)
+        plt.subplot(2, 2, 3)
+        make_line_plot('addModel', time, add_model)
+        plt.subplot(2, 2, 4)
+        make_line_plot('antiSpike', time, result)
+
+    def __harm_and_spikes_add():
+        data1 = Model.harm(N, A_0)
+        data2 = Model.spikes(N, M, 10*R, R_s)
+        add_model = Model.addModel(data1, data2)
+        result = Processing.antiSpike(add_model, N, R)
+
+        plt.subplot(2, 2, 1)
+        make_line_plot('Harm', time, data1)
+        plt.subplot(2, 2, 2)
+        make_line_plot('Spikes', time, data2)
+        plt.subplot(2, 2, 3)
+        make_line_plot('addModel', time, add_model)
+        plt.subplot(2, 2, 4)
+        make_line_plot('antiSpike', time, result)
+
+    N = 1000
+    M = 1
+    R = 100
+    R_s = 100
+    A_0 = 100
+    dt = 0.001
+    time = np.arange(0, N*dt, dt)
+
+    inner_funcs = [__noise_and_spikes_add, __harm_and_spikes_add]
+    for i in range(len(inner_funcs)):
+        inner_funcs[i]()
+        if i != len(inner_funcs) - 1:
+            plt.tight_layout()
+            fig.savefig(f"./plots/{title}_{i+1}.png")
+            current_title = fig._suptitle.get_text()
+            fig = plt.figure(figsize=(14, 8))
+            fig.suptitle(current_title)
+
+
+if __name__ == '__main__':
+    sns.set_theme(style="whitegrid")
+    if not os.path.exists("./plots"):
+        os.makedirs("./plots")
+
+    test_methods = {
+        test_antiSpike_method: 'Anti spike',
+    }
+
+    for test_method, title in test_methods.items():
+        fig = plt.figure(figsize=(14, 8))
+        fig.suptitle(title)
+        test_method()
+        plt.tight_layout()
+        fig.savefig(f"./plots/{title}.png")
+    plt.show()
---
+++ Lab8/src/processing.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Processing:
+    @staticmethod
+    def antiSpike(data, N, R):
+        """
+        Функция обнаружения и удаления выбросов за пределами диапазона R.
+        Заменяет выбросы линейной интерполяцией.
+        """
+        processed_data = np.copy(data)
+        spikes = np.abs(data) > R
+        for i in range(1, N - 1):
+            if spikes[i]:
+                processed_data[i] = (data[i - 1] + data[i + 1]) / 2
+        return processed_data
