5c09d5819c77215f28bffc62f1651bfa38973a3b
Artem Kurdikov <kourtema@gmail.com>
2024-12-01 15:06:28 +0300

    Lab11: implemented all tasks

---
+++ Lab11/src/processing.py
@@ -0,0 +1,59 @@
+import numpy as np
+from model import Model
+
+class Processing:
+    @staticmethod
+    def antiSpike(data, N, R):
+        """
+        Функция обнаружения и удаления выбросов за пределами диапазона R.
+        Заменяет выбросы линейной интерполяцией.
+        """
+        processed_data = np.copy(data)
+        spikes = np.abs(data) > R
+        for i in range(1, N - 1):
+            if spikes[i]:
+                processed_data[i] = (data[i - 1] + data[i + 1]) / 2
+        return processed_data
+
+    @staticmethod
+    def antiTrendLinear(data, N):
+        """
+        Функция удаления линейного тренда с помощью первой производной.
+        Возвращает данные с удалённым линейным трендом.
+        """
+        return np.diff(data, append=data[0])
+
+    @staticmethod
+    def antiTrendNonLinear(data, N, W):
+        """
+        Функция удаления нелинейного тренда методом скользящего среднего.
+        Возвращает данные с удалённым нелинейным трендом.
+        """
+        # Скользящее среднее для устранения тренда
+        trend = np.convolve(data, np.ones(W)/W, mode='valid')
+
+        # Для выравнивания размера массивов дополним trend до размера N
+        trend_padded = np.concatenate((trend, np.zeros(N - len(trend))))
+
+        # Убираем тренд из данных
+        detrended_data = data - trend_padded
+        return detrended_data
+
+    @staticmethod
+    def antiShift(data):
+        mean_value = np.mean(data)
+        proc_data = data - mean_value
+        return proc_data
+
+    @staticmethod
+    def antiNoise(data, M):
+        if M == 1:
+            return data
+
+        std_devs = []
+        for m in range(M):
+            # Поэлементное осреднение
+            avg_noise = np.mean(data[:m, :], axis=0)
+            # Стандартное отклонение осреднённого шума
+            std_devs.append(np.std(avg_noise))
+        return std_devs
